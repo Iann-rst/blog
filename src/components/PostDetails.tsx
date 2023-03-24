ca3ec6da4de72ff0fff754414115c9510613c53d
Iann Rodrigues <iannrst11@gmail.com>
2023-03-24 19:00:03 -0300

    feat: Detalhes do post (titulo, autor e conteúdo)

---
+++ src/components/PostDetails.tsx
@@ -0,0 +1,25 @@
+
+interface Props {
+  title: string;
+  author: string;
+  content: string;
+}
+
+export function PostDetails({ title, author, content }: Props) {
+  return (
+    <>
+      <header className="flex flex-col text-center">
+        <h2 title="Titulo da publicação" className="text-[#e1e1e1] font-bold text-4xl md:text-7xl">
+          {title}
+        </h2>
+
+        <span title="Autor" className="text-[#8d8d8d] text-xs md:text-base mt-2">
+          <em>{author}</em>
+        </span>
+      </header>
+      <p className="text-[#e1e1e1] text-center text-base md:text-xl mb-2">
+        {content}
+      </p>
+    </>
+  )
+}
