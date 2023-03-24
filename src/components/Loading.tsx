7f98ec7192315b65654339b76cc200562a413edb
Iann Rodrigues <iannrst11@gmail.com>
2023-03-24 19:00:27 -0300

    feat: Componente de Loading (Spinner)

---
+++ src/components/Loading.tsx
@@ -0,0 +1,7 @@
+export function Loading() {
+  return (
+    <div
+      className="inline-block h-8 w-8 animate-spin rounded-full border-[#00B37E] border-4 border-solid  border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+    />
+  )
+}
