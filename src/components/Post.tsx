4a70913c87d7c842cf53de95780d61b6111ceb66
Iann Rodrigues <iannrst11@gmail.com>
2023-03-25 17:16:04 -0300

    feat: style-focus

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -13,14 +13,14 @@ export function Post({ title, body, postsDetails }: PostProps) {
     >
       <button
         onClick={postsDetails}
-        className='w-full border border-[#24ac83] p-4 rounded-lg focus:outline-none focus:ring-2 focus:ring-secondary focus:ring-offset-2 focus:ring-offset-background'
+        className='group w-full border border-[#24ac83] p-4 rounded-lg focus:outline-none focus:ring-2 focus:ring-secondary focus:ring-offset-2 focus:ring-offset-background'
       >
         <div className='flex flex-col'>
-          <strong className='text-lg text-center md:text-start normal-case group-hover:text-secondary transition-colors ease-in duration-[0.3s]'>
+          <strong className='text-lg text-center md:text-start normal-case group-hover:text-secondary group-focus:text-secondary transition-colors ease-in duration-[0.3s]'>
             {title}
           </strong>
 
-          <p className='mt-4 text-justify group-hover:text-secondary transition-colors ease-in duration-[0.3s]'>
+          <p className='mt-4 text-justify group-hover:text-secondary group-focus:text-secondary transition-colors ease-in duration-[0.3s]'>
             {body}
           </p>
         </div>
