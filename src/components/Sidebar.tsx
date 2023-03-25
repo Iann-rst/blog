2c496524c82ca4acd48b232e054ae521c76a7a62
Iann Rodrigues <iannrst11@gmail.com>
2023-03-25 17:16:56 -0300

    feat: Sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,9 +3,10 @@ export function Sidebar() {
   return (
     <aside className='w-full bg-[#202024] rounded-lg overflow-hidden '>
       <img className='w-full h-[70px] object-cover' src="https://images.unsplash.com/photo-1617042375876-a13e36732a04?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" alt="" />
-      <div className='flex flex-col justify-center items-center p-4 mt-4'>
-        <strong className="text-primary">Bem-vindo ao blog!</strong>
-        <span className='text-sm text-primary text-center'>Blog feito para teste de front-end.</span>
+      <div className='flex flex-col justify-center items-center p-4 mt-4 gap-2'>
+        <strong className="text-primary">Iann Rodrigues</strong>
+        <span className='text-sm text-primary text-center'>Criação de um site para uma API RESTFul</span>
+        <span className='text-xs text-primary font-bold text-center'>Listagem de posts e comentários</span>
       </div>
     </aside>
   )
