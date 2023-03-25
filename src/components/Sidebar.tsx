3b633e81d237162a79a443b42ff52b6f45a652b9
Iann Rodrigues <iannrst11@gmail.com>
2023-03-25 18:26:12 -0300

    fix: Sidebar img

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,10 @@
 
+import imgSidebar from '../assets/image_sidebar.jpg'
+
 export function Sidebar() {
   return (
     <aside className='w-full bg-[#202024] rounded-lg overflow-hidden '>
-      <img className='w-full h-[70px] object-cover' src="https://images.unsplash.com/photo-1617042375876-a13e36732a04?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" alt="" />
+      <img className='w-full h-[70px] object-cover' src={imgSidebar} alt="" />
       <div className='flex flex-col justify-center items-center p-4 mt-4 gap-2'>
         <strong className="text-primary">Iann Rodrigues</strong>
         <span className='text-sm text-primary text-center'>Criação de um site para uma API RESTFul</span>
